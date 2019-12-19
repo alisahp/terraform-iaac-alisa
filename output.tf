b1a6c9f236ee64fe91302c482b2bbf1660a1e657
Ali <alisevhp@gmail.com>
2019-12-18 19:12:12 -0600

    output edited

--- output.tf
+++ output.tf
@@ -1,3 +1,24 @@
 output "bucket_name" {
   value = "${aws_s3_bucket.b.bucket}"
 }
+
+
+output "sec_group" {
+  value = aws_security_group.allow_ssh.name
+}
+
+output "sec_group" {
+  value = aws_key_pair.deployer.key_name
+}
+
+
+output "username" {
+  value = "some_username"
+}
+
+output "password" {
+  value = "password"
+}
+
+
+
