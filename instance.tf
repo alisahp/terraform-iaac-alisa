04c2b37ecfa9076e573d2f39ce29d0d40e61e659
Ali <alisevhp@gmail.com>
2019-12-21 12:32:53 -0600

    fixed

--- instance.tf
+++ instance.tf
@@ -1,34 +1,27 @@
 resource "aws_instance" "web" {
-  count           = var.count_instance
-  ami             = var.ami 
+  count = var.count_instance
+  ami             = var.ami
   instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
-  key_name        = aws_key_pair.deployer.key_name
+  key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      type = "ssh"
+      user = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+        ]
+      }
   lifecycle{
     prevent_destroy = false
   }
-   provisioner "remote-exec" { 
-    connection { 
-      host        = self.public_ip 
-      type        = "ssh" 
-      user        = var.user 
-      private_key = file(var.ssh_key_location)
-    } 
-    inline = [ 
-      "sudo yum install -y epel-release", 
-      "sudo yum install httpd -y" ,
-      "systemctl start httpd",
-      "systemctl install -ms -y",
-    ] 
-  } 
   tags = {
     Name = "HelloWorld${count.index +1}"
   }
-}
-resource "aws_instance" "imported" {
-  ami             = "ami-00068cd7555f543d5"
-  key_name        = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh"]
-  instance_type  = "t2.micro"
 }
