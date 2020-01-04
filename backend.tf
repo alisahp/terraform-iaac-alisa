be1400948b6ba145d1b5217c0957b3fce819efef
Ali <alisevhp@gmail.com>
2020-01-04 11:04:44 -0600

    as

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-test-bucket-alisait"
+    bucket = "terraform-us-virgin"
     key    = "aws/iam/iam_state"
-    region = "eu-west-1"
+    region = "us-east-1"
   }
 }
