4701d234738a32575213df62c6578517bb730dba
Ali <alisevhp@gmail.com>
2020-01-06 20:52:29 -0600

    folders was put to old

--- Application/variable.tf
+++ Application/variable.tf
@@ -2,19 +2,12 @@ variable "name" {
   default = "wordpress1"
 }
 
-variable "cidr" {
-}
+variable "cidr" {}
 
-variable "azs1" {
-  
-}
+variable "azs1" {}
 
-variable "azs2" {
-  
-}
+variable "azs2" {}
 
-variable "azs3" {
-  
-}
+variable "azs3" {}
 
 
