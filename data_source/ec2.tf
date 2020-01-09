73a40e9a4a252522ef78ab00b085a470afe1a7e8
Ali <alisevhp@gmail.com>
2020-01-08 20:03:07 -0600

    as

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -23,13 +23,13 @@ data "aws_ami" "ubuntu" {
 
 # Show the AMI id
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}" 
+  value = data.aws_ami.ubuntu.id
 }
 
 
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
