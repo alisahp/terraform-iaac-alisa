53452190067617d00d8ff0bff77220dd1daecedd
Ali <alisevhp@gmail.com>
2020-01-08 18:46:13 -0600

    s

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,13 +1,13 @@
 # Go to Region us-east-2
 provider "aws" {
-  region = "us-east-2" 
+  region = "us-east-2" 
 }
 
 
 
 # Search for Ubuntu Latest with the owner
 data "aws_ami" "ubuntu" {
-  most_recent = true 
+  most_recent = true 
   owners = ["099720109477"]
 }
 
@@ -15,5 +15,5 @@ data "aws_ami" "ubuntu" {
 
 # Show the AMI id
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}" 
+  value = "${data.aws_ami.ubuntu.id}" 
 }
