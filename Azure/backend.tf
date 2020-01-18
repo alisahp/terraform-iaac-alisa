302873f789e9e4d55bc1300c00f2b1eff5c284d2
Ali <alisevhp@gmail.com>
2020-01-18 12:52:07 -0600

    blob & (s3 in aws) in Azure added

---
+++ Azure/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "alisait1"
+    container_name       = "alisaitcontainer"
+    key                  = "prod.terraform.tfstate"
+  }
